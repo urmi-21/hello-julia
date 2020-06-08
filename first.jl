7c30cb7dd9b5de1c1d8b32262704cc978ef63403
urmi-21 <mrbains21@gmail.com>
2020-06-08 17:58:42 -0500

    update

---
+++ first.jl
@@ -0,0 +1,19 @@
+#%%
+println("hello")
+println("Julia")
+#%%
+
+#%%
+num1=22
+num2=22.35
+println("n1: ",num1," ",typeof(num1))
+println("n2: ",num2," ",typeof(num2))
+d=num1/num2
+println(d)
+strA="myString"
+println(typeof(strA))
+
+num3=convert(Float64,num1)
+println("n3: ",num3," ",typeof(num1))
+
+#%%
